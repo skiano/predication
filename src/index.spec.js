a3703f628db97580eea1360280268daaba93ee62
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 14:29:08 -0800

    adding tests for not opperator and composition

--- src/index.spec.js
+++ src/index.spec.js
@@ -81,6 +81,24 @@ test('Or Operator', t => {
   });
 });
 
+test('Not Operator', t => {
+  t.plan(2);
+
+  const interpreter = {
+    bool: (model, value) => value
+  }
+
+  t.equal(aon(
+    ['!', ['bool', true]],
+    interpreter)(), false, 'Not(true) === false');
+
+  t.equal(aon(
+    ['!', ['bool', false]],
+    interpreter)(), true, 'Not(true) === false');
+});
+
+// TODO: Not opperator
+
 test('Interpreter', t => {
   t.plan(4);
 
@@ -103,6 +121,42 @@ test('Interpreter', t => {
   }
 
   aon(['!', ['test', 1, 2, 3, 4]], interpreter)({model: true});  
+});
+
+test('Composition', t => {
+  t.plan(6);
+
+  const interpreter = {
+    lessThan: (model, key, threshold) => {
+      return model[key] < threshold;
+    },
+    divisibleBy: (model, key, modulus) => {
+      return model[key] % modulus === 0;
+    }
+  }
+
+  const data = ['&&', ['lessThan', 'v', 15],
+                      ['!', ['lessThan', 'v', 5]],
+                      ['||', ['divisibleBy', 'v', 2],
+                             ['divisibleBy', 'v', 3]]]
+
+  t.equal(aon(data, interpreter)({v: 6}), true,
+    '6 is: less than 15, and not less than 5, and divisible by either 2 or 3');
+
+  t.equal(aon(data, interpreter)({v: 8}), true,
+    '8 is: less than 15, and not less than 5, and divisible by either 2 or 3');
+
+  t.equal(aon(data, interpreter)({v: 9}), true,
+    '9 is: less than 15, and not less than 5, and divisible by either 2 or 3');
+
+  t.equal(aon(data, interpreter)({v: 7}), false,
+    '7 is not: less than 15, and not less than 5, and divisible by either 2 or 3');
+
+  t.equal(aon(data, interpreter)({v: 3}), false,
+    '3 is not: less than 15, and not less than 5, and divisible by either 2 or 3');
+
+  t.equal(aon(data, interpreter)({v: 17}), false,
+    '17 is not: less than 15, and not less than 5, and divisible by either 2 or 3');
 });
 
 // TODO: Test how `this` is affected
