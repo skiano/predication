1a373b9b126b0e28ecb0ea6e9cfd1f1466775876
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 08:05:27 -0800

    cleaning

--- src/example.js
+++ src/example.js
@@ -1,28 +1,11 @@
 
-
-const logic = ['&&', x => x > 50, 
-                     x => x < 150,
-                     ['||', x => x % 2 === 0,
-                            x => x % 3 === 0]]
-
-const predicate = makePredicate(logic);
-
-console.log(predicate(60));
-console.log(predicate(61));
-
-// change term to opperand
-// how to take care of security
-// right now this executes arbitrary functions
-// i supose it needs to go al the way to the level of taking a data object??
-// maybe even then it cant be safe
-
 function logicToJs(logic, commands) {
   const chunk = logic.slice(1)
-                     .map(term => {
-                        if (Array.isArray(term)) {
-                          return logicToJs(term, commands);
+                     .map(opperand => {
+                        if (Array.isArray(opperand)) {
+                          return logicToJs(opperand, commands);
                         } else {
-                         return `(x[${commands.push(term)}])(v)`;
+                         return `(x[${commands.push(opperand)}])(v)`;
                         }
                       })
                      .join(` ${logic[0]} `);
@@ -35,3 +18,22 @@ function makePredicate(logic) {
     return eval(logicToJs(logic, x));
   };
 }
+
+// TODO: handle ! not opperator
+// change opperand to opperand
+// how to take care of security
+// right now this executes arbitrary functions
+// i supose it needs to go al the way to the level of taking a data object??
+// maybe even then it cant be safe
+// maybe it is safe though because this creates the string
+// its not as if it accepts a string as an arg and executes it
+
+const logic = ['&&', x => x > 50,
+                     x => x < 150,
+                     ['||', x => x % 2 === 0,
+                            x => x % 3 === 0]]
+
+const predicate = makePredicate(logic);
+
+console.log(predicate(60));
+console.log(predicate(61));
