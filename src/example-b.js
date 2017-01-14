3107b9f8725bee4d3c9e94e7a02b32647960e18c
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 00:50:58 -0800

    tweaking example

--- src/example-b.js
+++ src/example-b.js
@@ -4,7 +4,7 @@ import { and, or, not, createAndOrNot } from './';
 const a = x => x % 2 === 0;
 const b = x => x % 3 === 0;
 const c = x => x % 4 === 0;
-const d = x => x > 20;
+const d = x => x < 20;
 
 const check = and(a, or(b, c), not(d));
 
