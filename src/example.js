c4ac08001ec1331d95a792ed9c7efd9f1b39ec74
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 09:22:50 -0800

    preparing for future logical opperators

--- src/example.js
+++ src/example.js
@@ -1,29 +1,43 @@
 
-function dataToJs(data, commands) {
-  if (data[0] === '!') {
-    /*
-     * Handle Not
-     */
-    return `!x[${commands.push(data[1]) - 1}](v)`;
-  } else {
-    /*
-     * Handle And / Or
-     */
-    return `(${data.slice(1)
-      .map(opperand => {
-        if (Array.isArray(opperand)) {
-          return dataToJs(opperand, commands);
-        } else {
-         return `x[${commands.push(opperand) - 1}](v)`;
-        }
-      }).join(` ${data[0]} `)})`;
+// look into boolean algebra
+// https://en.wikipedia.org/wiki/Boolean_algebra#Values
+// might be useful to implement XOR and equivilence etc
+
+function useInterpreter(operand, interpreter) {
+  return v => interpreter[operand[0]](v, ...operand.slice(1));
+}
+
+const LOGICAL_OPPERATORS = /^(!|&&|\|\|)$/;
+
+function dataToJs(statement, interpreter, commands) {
+  const opperator = statement[0];
+  const opperands = statement.slice(1);
+
+  switch (true) {
+    case (opperator === '!'):
+      return `!c[${commands.push(useInterpreter(opperands[0], interpreter)) - 1}](v)`;
+
+    case (opperator === '&&' || opperator === '||'):
+      return `(${opperands
+        .map(statement => {
+          if (LOGICAL_OPPERATORS.test(statement[0])) {
+            return dataToJs(statement, interpreter, commands);
+          } else {
+           return `c[${commands.push(useInterpreter(statement, interpreter)) - 1}](v)`;
+          }
+        }).join(` ${opperator} `)})`;
+
+    default:
+      throw new Error('unrecognized opperator: ' + opperator);
+
   }
 }
 
-function makePredicate(data) {
-  const x = [];
+function makePredicate(statement, interpreter) {
+  const c = []; // commands
   return (v) => {
-    return eval(dataToJs(data, x));
+    console.log(dataToJs(statement, interpreter, c));
+    return eval(dataToJs(statement, interpreter, c));
   };
 }
 
@@ -37,14 +51,25 @@ function makePredicate(data) {
 // its not as if it accepts a string as an arg and executes it
 
 
-const data = ['&&', ['!', () => false], ['||', () => false, () => true]];
+// const data = ['&&', ['!', () => false], ['||', () => false, () => true]];
 
-const dataData = ['&&', ['GREATER_THAN', 'cost', 50],
-                        ['LESS_THAN', 'cost', 150],
-                        ['||', ['DIVISIBLE_BY', 'cost', 2],
-                               ['DIVISIBLE_BY', 'cost', 3]]]
+// const dataData = ['&&', ['GREATER_THAN', 'cost', 50],
+//                         ['LESS_THAN', 'cost', 150],
+//                         ['||', ['DIVISIBLE_BY', 'cost', 2],
+//                                ['DIVISIBLE_BY', 'cost', 3]]]
 
-const predicate = makePredicate(data);
 
-console.log(predicate(60));
-// console.log(predicate(61));
+// const data = ['!', ['greaterThan', 'value', 50]];
+const data = ['||', ['greaterThan', 'value', 80], ['greaterThan', 'value', 60]];
+
+const interpreter = {
+  greaterThan(model, key, value) {
+    return model[key] > value;
+  },
+  lessThan(model, key, value) {
+    return model[key] < value;
+  }
+}
+
+const predicate = makePredicate(data, interpreter);
+console.log(predicate({value: 70}));
