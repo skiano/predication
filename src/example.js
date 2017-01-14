8065ad15ead8c7ca91efed24f2ed645bb6e121d3
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 07:24:15 -0800

    adding in idea of command register so it doesnt create new functions

--- src/example.js
+++ src/example.js
@@ -8,18 +8,30 @@ const logic = ['&&', x => x > 50,
 const predicate = makePredicate(logic);
 
 console.log(predicate(60));
+console.log(predicate(61));
 
+// change term to opperand
+// how to take care of security
+// right now this executes arbitrary functions
+// i supose it needs to go al the way to the level of taking a data object??
+// maybe even then it cant be safe
 
-function logicToJs(logic) {
+function logicToJs(logic, commands) {
   const chunk = logic.slice(1)
-                     .map(term => Array.isArray(term) ? 
-                        logicToJs(term) : `(${term.toString()})(v)`)
+                     .map(term => {
+                        if (Array.isArray(term)) {
+                          return logicToJs(term, commands);
+                        } else {
+                         return `(x[${commands.push(term)}])(v)`;
+                        }
+                      })
                      .join(` ${logic[0]} `);
   return `(${chunk})`;
 }
 
 function makePredicate(logic) {
+  const x = [];
   return (v) => {
-    return eval(logicToJs(logic));
+    return eval(logicToJs(logic, x));
   };
 }
