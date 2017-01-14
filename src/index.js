a5de15cb17c8709cc072ccbde91cc3f70abe7c75
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-14 00:49:12 -0800

    fixing bugs with not

--- src/index.js
+++ src/index.js
@@ -12,17 +12,18 @@ const CHECK_ERR = 'checks must be functions';
 const NOT_ERROR = 'not() requires exactly one argument';
 
 function createAndOrNot(errorHandler) {
-  const handleError = (fn, value) => {
+  const callSafely = (fn, value) => {
     if (typeof fn !== 'function') {
       throw new Error(CHECK_ERR);
     } else {
       try {
         return fn(value);
-      } catch (e) {
+      } catch (err) {
+        console.log(err)
         if (typeof errorHandler === 'function') {
           // allow user to handle the error
           // and pass them the original value
-          return errorHandler(e, value);
+          return errorHandler(err, value);
         }
         return false;
       } 
@@ -33,7 +34,7 @@ function createAndOrNot(errorHandler) {
     and(...checks) {
       return value => {
         for (let check of checks) {
-          if (!handleError(check, value)) return false;
+          if (!callSafely(check, value)) return false;
         }
         return true;
       };
@@ -42,17 +43,16 @@ function createAndOrNot(errorHandler) {
     or(...checks) {
       return value => {
         for (let check of checks) {
-          if (handleError(check, value)) return true;
+          if (callSafely(check, value)) return true;
         }
         return false;
       };
     },
 
     not(...checks) {
-      if (checks.length > 1) {
-        throw new Error(NOT_ERROR);
-      } else {
-        return !handleError(checks[0](x));  
+      if (checks.length > 1) throw new Error(NOT_ERROR);
+      return value => {
+        return !callSafely(checks[0], value);
       }
     },
   };
