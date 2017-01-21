5b22118e2dfdccd52cdfa19df94d9c46f35c195e
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-21 00:24:21 -0800

    new var name

--- src/index.spec.js
+++ src/index.spec.js
@@ -9,8 +9,8 @@ test('Composition', t => {
                       not($lt(5)),
                       or($mod(2), $mod(3)));
 
-  const oneToTwenty = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
-  const matches = oneToTwenty.filter(predicate);
+  const values = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
+  const matches = values.filter(predicate);
 
   t.deepLooseEqual(matches,
     [ 6, 8, 9, 10, 12, 14 ],
