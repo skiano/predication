bf012fc2f33d97c0bcb2c039cf50d861a12f0c0a
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-21 00:14:34 -0800

    getting rid of bad merge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-
-// look into boolean algebra
-// https://en.wikipedia.org/wiki/Boolean_algebra#Values
-// might be useful to implement XOR and equivilence etc
-
-import aon from './aon';
-export { aon };
-export * from './aon.compose';
-
-=======
 export * from './and-or-not';
 export * from './common-predicates';
->>>>>>> f2
