ebeed82bfcfd5c1443523dc7d4aadd7994eeeed8
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-21 00:11:38 -0800

    putting in a passing test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,27 +1,18 @@
 import test from 'tape';
+import { and, or, not } from './';
+import { $lt, $mod } from './';
 
 test('Composition', t => {
   t.plan(1);
 
-  const interpreter = {
-    lessThan: (model, key, threshold) => {
-      return model[key] < threshold;
-    },
-    divisibleBy: (model, key, modulus) => {
-      return model[key] % modulus === 0;
-    }
-  }
+  const predicate = and($lt(15), 
+                      not($lt(5)),
+                      or($mod(2), $mod(3)));
 
-  const data = ['&&', ['lessThan', 'v', 15],
-                      ['!', ['lessThan', 'v', 5]],
-                      ['||', ['divisibleBy', 'v', 2],
-                             ['divisibleBy', 'v', 3]]];
-
-  const predicate = aon(data, interpreter);
-  const oneToFifteen = Array.from(Array(20).keys()).map(v => ({v: v + 1}));
+  const oneToFifteen = Array.from(Array(20).keys());
   const matches = oneToFifteen.filter(predicate);
 
   t.deepLooseEqual(matches,
-    [ { v: 6 }, { v: 8 }, { v: 9 }, { v: 10 }, { v: 12 }, { v: 14 } ],
+    [ 6, 8, 9, 10, 12, 14 ],
     'filter: less than 15, and not less than 5, and divisible by either 2 or 3')
 });
