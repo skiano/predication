3f926e5d80bbcec8cd44aa6e41e8832ffbe312d6
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-21 09:59:10 -0800

    removing unneded parens

--- src/and-or-not.js
+++ src/and-or-not.js
@@ -1,7 +1,7 @@
 const makeOperator = (logic) => {
   return (...predicates) => {
     const fn = [];
-    const js = `!!(${predicates.map((p) => (
+    const js = `!!(${predicates.map(p => (
       `fn[${fn.push(p) - 1}](v)`
     )).join(logic)})`;
     return v => eval(js);
