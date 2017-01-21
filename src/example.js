8e872248c00ccb191b1bfcd8aa809b5e3d956e6a
Gregory Skiano <gregoryskiano@gmail.com>
2017-01-20 23:39:19 -0800

    working on library

--- src/example.js
+++ src/example.js
@@ -1,15 +1,34 @@
 
 import { and, or, not } from './';
 
-const a = x => x % 2 === 0;
-const b = x => x % 3 === 0;
-const c = x => x > 50;
-const d = x => x > 20;
+const $in = value => arr => Array.contains(arr, value);
+const $nin = value => arr => !Array.contains(arr, value);
+const $ne = value => key => key !== value;
+const $eq = value => key => key === value;
+const $lt = value => key => key < value;
+const $lte = value => key => key <= value;
+const $gt = value => key => key > value;
+const $gte = value => key => key >= value;
+const $mod = (divisor, remainder) => v => v % divisor === (remainder || 0);
 
-const invertedD = not(d);
+const $obj = (predicates) => model => {
+  for (let key in predicates) {
+    if (predicates.hasOwnProperty(key)) {
+      if (!predicates[key](model[key])) return false;
+    }
+  }
+  return true;
+}
 
-const comp = or(c, invertedD);
+const predicate = and($obj({x: $gt(5)}), 
+                      $obj({y: $gt(5)}));
 
-const predicate = and(a, b, comp);
+console.log(predicate({x: 13, y: 10}));
 
-console.log(predicate(60))
+
+// const predicate = and($mod(2),
+//                       $mod(3),
+//                       or($gt(50),
+//                          not($gt(20))));
+
+// console.log(predicate(60))
