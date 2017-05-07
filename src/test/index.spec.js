5d1607d7bb5575b03e4b1665529b7865372c6d6d
Gregory Skiano <gregoryskiano@gmail.com>
2017-05-06 19:07:41 -0700

    testing access

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -20,5 +20,24 @@ test('Logic', t => {
 
   t.deepLooseEqual(matches,
     [ 6, 8, 9, 10, 12, 14 ],
-    'filter: less than 15, and not less than 5, and divisible by either 2 or 3')
+    'filter: less than 15, and not less than 5, and divisible by either 2 or 3');
+});
+
+test('Logic Object Access', t => {
+  t.plan(5);
+
+  const p1 = predication({by: 'foo.bar[-0]', eq: true});
+  t.equal(p1({foo: {bar: [false, true]}}), true, 'logic: object access');
+  t.equal(p1({foo: {bar: [true, false]}}), false, 'logic: object access');
+
+  const p2 = predication({
+    by: 'foo',
+    or: [
+      {by: 'bar', eq: true},
+      {by: 'baz', eq: true}
+    ]
+  })
+  t.equal(p2({foo: {bar: true, baz: false}}), true, 'logic: object deep access');
+  t.equal(p2({foo: {baz: true}}), true, 'logic: object deep access or');
+  t.equal(p2({foo: {baz: false, bar: false}}), false, 'logic: object deep access or false');
 });
