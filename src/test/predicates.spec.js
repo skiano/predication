b344ea210d92c625f1029675804bea617b03785e
Gregory Skiano <gregoryskiano@gmail.com>
2017-05-09 10:36:43 -0700

    simplifying tests

--- src/test/predicates.spec.js
+++ src/test/predicates.spec.js
@@ -10,13 +10,13 @@ import {
 test('Predicates MGMT', t => {
   t.plan(5);
 
-  t.equal(Array.isArray(listPredicates()), true, 'list predicates');
+  t.true(Array.isArray(listPredicates()), 'list predicates');
 
   registerPredicate('foo', c => v => true);
 
-  t.equal(typeof getPredicate('foo') === 'function', true, 'register and get prdicate');
+  t.true(typeof getPredicate('foo') === 'function', 'register and get prdicate');
 
-  t.equal(hasPredicate('foo'), true, 'check for predicate');
+  t.true(hasPredicate('foo'), 'check for predicate');
 
   removePredicate('foo');
 
@@ -29,8 +29,8 @@ test('Predicates MGMT', t => {
 test('Predicates THIS', t => {
   t.plan(10);
 
-  t.equal(getPredicate('eq', 2, 'foo')({foo: 2}), true, 'simple this');
-  t.equal(getPredicate('eq', 2, 'foo.bar[0]')({foo: {bar: [2]}}), true, 'advanced this');
+  t.true(getPredicate('eq', 2, 'foo')({foo: 2}), 'simple this');
+  t.true(getPredicate('eq', 2, 'foo.bar[0]')({foo: {bar: [2]}}), 'advanced this');
   t.equal(getPredicate('eq', 2, 'foo.baz')({foo: {bar: [2]}}), undefined, 'bad this');
   t.equal(getPredicate('eq', 2, 'foo.baz')(), undefined, 'bad value');
 
@@ -39,20 +39,20 @@ test('Predicates THIS', t => {
   const both = getPredicate('and', [eq, gt], 'foo');
   const either = getPredicate('or', [eq, gt], 'foo');
 
-  t.equal(both({foo: {bar: true, baz: true}}), true, 'combination - and: true');
-  t.equal(both({foo: {bar: false, baz: true}}), false, 'combination - and: false');
-  t.equal(both({foo: {bar: true}}), false, 'combination - and: undefined');
+  t.true(both({foo: {bar: true, baz: true}}), 'combination - and: true');
+  t.false(both({foo: {bar: false, baz: true}}), 'combination - and: false');
+  t.false(both({foo: {bar: true}}), 'combination - and: undefined');
 
-  t.equal(either({foo: {bar: false, baz: true}}), true, 'combination - or: true');
-  t.equal(either({foo: {bar: false, baz: false}}), false, 'combination - or: false');
-  t.equal(either({foo: {baz: true}}), true, 'combination - or: undefined');
+  t.true(either({foo: {bar: false, baz: true}}), 'combination - or: true');
+  t.false(either({foo: {bar: false, baz: false}}), 'combination - or: false');
+  t.true(either({foo: {baz: true}}), 'combination - or: undefined');
 });
 
 test('Predicates THAT', t => {
   t.plan(4);
 
-  t.equal(getPredicate('eq', {that: 'bar'}, 'foo')({foo: 2, bar: 2}), true, 'simple that');
-  t.equal(getPredicate('eq', {that: 'bar[0]'}, 'foo')({foo: 2, bar: [2]}), true, 'advanced that');
+  t.true(getPredicate('eq', {that: 'bar'}, 'foo')({foo: 2, bar: 2}), 'simple that');
+  t.true(getPredicate('eq', {that: 'bar[0]'}, 'foo')({foo: 2, bar: [2]}), 'advanced that');
   t.equal(getPredicate('eq', {that: 'bar[0].baz'}, 'foo')({foo: 2, bar: [2]}), undefined, 'bad that');
   t.equal(getPredicate('eq', {that: 'bar[0].baz'}, 'foo')(), undefined, 'bad value');
 });
@@ -63,89 +63,90 @@ test('Predicates NOT with undefined', t => {
   const EQ = getPredicate('eq', 2, 'foo.baz');
   const notEQ = getPredicate('not', EQ);
 
-  t.equal(EQ({foo: {baz: 2}}), true, 'equal');
-  t.equal(notEQ({foo: {baz: 2}}), false, 'not equal');
-  t.equal(notEQ({foo: {baz: 4}}), true, 'not not equal');
-  t.equal(notEQ({bar: true}), false, 'not not equal');
+  t.true(EQ({foo: {baz: 2}}), 'equal');
+  t.false(notEQ({foo: {baz: 2}}), 'not equal');
+  t.true(notEQ({foo: {baz: 4}}), 'not not equal');
+  t.false(notEQ({bar: true}), 'not not equal');
 });
 
 test('Predicates', t => {
-  t.plan(58)
+  t.plan(59)
 
-  t.equal(getPredicate('exists', true)(0), true, 'exists: true for 0');
-  t.equal(getPredicate('exists', true)(''), true, 'exists: true for empty string');
-  t.equal(getPredicate('exists', true)(false), true, 'exists: true for false');
-  t.equal(getPredicate('exists', true)(null), true, 'exists: true for null');
-  t.equal(getPredicate('exists', true)(undefined), false, 'exists: false for undefined');
+  t.true(getPredicate('exists', true)(0), 'exists: true for 0');
+  t.true(getPredicate('exists', true)(''), 'exists: true for empty string');
+  t.true(getPredicate('exists', true)(false), 'exists: true for false');
+  t.true(getPredicate('exists', true)(null), 'exists: true for null');
+  t.false(getPredicate('exists', true)(undefined), 'exists: false for undefined');
 
-  t.equal(getPredicate('not', () => false)(true), true, 'not: true');
+  t.true(getPredicate('not', () => false)(1), 'not: true');
+  t.false(getPredicate('not', () => true)(1), 'not: true');
 
-  t.equal(getPredicate('or', [() => true, () => false])(1), true, 'or: true early');
-  t.equal(getPredicate('or', [() => false, () => true])(1), true, 'or: true late');
-  t.equal(getPredicate('or', [() => false, () => false])(1), false, 'or: false');
+  t.true(getPredicate('or', [() => true, () => false])(1), 'or: true early');
+  t.true(getPredicate('or', [() => false, () => true])(1), 'or: true late');
+  t.false(getPredicate('or', [() => false, () => false])(1), 'or: false');
 
-  t.equal(getPredicate('and', [() => false, () => true])(1), false, 'and: false early');
-  t.equal(getPredicate('and', [() => true, () => false])(1), false, 'and: false late');
-  t.equal(getPredicate('and', [() => true, () => true])(1), true, 'and: true');
-  t.equal(getPredicate('and', [() => true, () => undefined])(1), false, 'and: handle undefined');
+  t.false(getPredicate('and', [() => false, () => true])(1), 'and: false early');
+  t.false(getPredicate('and', [() => true, () => false])(1), 'and: false late');
+  t.true(getPredicate('and', [() => true, () => true])(1), 'and: true');
+  t.false(getPredicate('and', [() => true, () => undefined])(1), 'and: handle undefined');
 
-  t.equal(getPredicate('eq', 2)(2), true, 'equals: true');
-  t.equal(getPredicate('eq', 2)(3), false, 'equals: false');
+  t.true(getPredicate('eq', 2)(2), 'equals: true');
+  t.false(getPredicate('eq', 2)(3), 'equals: false');
 
-  t.equal(getPredicate('ne', 2)(3), true, 'not equals: true');
-  t.equal(getPredicate('ne', 2)(2), false, 'not equals: false');
+  t.true(getPredicate('ne', 2)(3), 'not equals: true');
+  t.false(getPredicate('ne', 2)(2), 'not equals: false');
 
-  t.equal(getPredicate('in', [1, 2])(2), true, 'includes: (array) true');
-  t.equal(getPredicate('in', [1, 2])(3), false, 'includes: (array) false');
+  t.true(getPredicate('in', [1, 2])(2), 'includes: (array) true');
+  t.false(getPredicate('in', [1, 2])(3), 'includes: (array) false');
 
-  t.equal(getPredicate('in', 'abc')('bc'), true, 'includes: (string) true');
-  t.equal(getPredicate('in', 'aBc')('AbC'), true, 'includes: (string: case insensitive) true');
-  t.equal(getPredicate('in', 'abc')('ac'), false, 'includes: (string) false');
+  t.true(getPredicate('in', 'abc')('bc'), 'includes: (string) true');
+  t.true(getPredicate('in', 'aBc')('AbC'), 'includes: (string: case insensitive) true');
+  t.false(getPredicate('in', 'abc')('ac'), 'includes: (string) false');
 
-  t.equal(getPredicate('nin', [1, 2])(3), true, 'does not include: (array) true');
-  t.equal(getPredicate('nin', [1, 2])(2), false, 'does not include: (array) false');
+  t.true(getPredicate('nin', [1, 2])(3), 'does not include: (array) true');
+  t.false(getPredicate('nin', [1, 2])(2), 'does not include: (array) false');
 
-  t.equal(getPredicate('lt', 0)(-1), true, 'less than: less');
-  t.equal(getPredicate('lt', 0)(0), false, 'less than: equal');
-  t.equal(getPredicate('lt', 0)(1), false, 'less than: more');
+  t.true(getPredicate('lt', 0)(-1), 'less than: less');
+  t.false(getPredicate('lt', 0)(0), 'less than: equal');
+  t.false(getPredicate('lt', 0)(1), 'less than: more');
 
-  t.equal(getPredicate('gt', 0)(-1), false, 'greater than: less');
-  t.equal(getPredicate('gt', 0)(0), false, 'greater than: equal');
-  t.equal(getPredicate('gt', 0)(1), true, 'greater than: more');
+  t.false(getPredicate('gt', 0)(-1), 'greater than: less');
+  t.false(getPredicate('gt', 0)(0), 'greater than: equal');
+  t.true(getPredicate('gt', 0)(1), 'greater than: more');
 
-  t.equal(getPredicate('lte', 0)(-1), true, 'less than or equal: less');
-  t.equal(getPredicate('lte', 0)(0), true, 'less than or equal: equal');
-  t.equal(getPredicate('lte', 0)(1), false, 'less than or equal: more');
+  t.true(getPredicate('lte', 0)(-1), 'less than or equal: less');
+  t.true(getPredicate('lte', 0)(0), 'less than or equal: equal');
+  t.false(getPredicate('lte', 0)(1), 'less than or equal: more');
 
-  t.equal(getPredicate('gte', 0)(-1), false, 'greater than or equal: less');
-  t.equal(getPredicate('gte', 0)(0), true, 'greater than or equal: equal');
-  t.equal(getPredicate('gte', 0)(1), true, 'greater than or equal: more');
+  t.false(getPredicate('gte', 0)(-1), 'greater than or equal: less');
+  t.true(getPredicate('gte', 0)(0), 'greater than or equal: equal');
+  t.true(getPredicate('gte', 0)(1), 'greater than or equal: more');
 
-  t.equal(getPredicate('rng', [0, 10])(5), true, 'range: within');
-  t.equal(getPredicate('rng', [0, 10])(10), true, 'range: max');
-  t.equal(getPredicate('rng', [0, 10])(0), true, 'range: min');
-  t.equal(getPredicate('rng', [0, 10])(-1), false, 'range: too low');
-  t.equal(getPredicate('rng', [0, 10])(11), false, 'range: too high');
+  t.true(getPredicate('rng', [0, 10])(5), 'range: within');
+  t.true(getPredicate('rng', [0, 10])(10), 'range: max');
+  t.true(getPredicate('rng', [0, 10])(0), 'range: min');
+  t.false(getPredicate('rng', [0, 10])(-1), 'range: too low');
+  t.false(getPredicate('rng', [0, 10])(11), 'range: too high');
 
-  t.equal(getPredicate('mod', 2)(4), true, 'divisible this: true');
-  t.equal(getPredicate('mod', 3)(4), false, 'divisible this: false');
+  t.true(getPredicate('mod', 2)(4), 'divisible this: true');
+  t.false(getPredicate('mod', 3)(4), 'divisible this: false');
 
-  t.equal(getPredicate('mod', [3,0])(3), true, 'divisible this: (remainder) 0');
-  t.equal(getPredicate('mod', [3,1])(4), true, 'divisible this: (remainder) 1');
-  t.equal(getPredicate('mod', [3,2])(5), true, 'divisible this: (remainder) 2');
+  t.true(getPredicate('mod', [3,0])(3), 'divisible this: (remainder) 0');
+  t.true(getPredicate('mod', [3,1])(4), 'divisible this: (remainder) 1');
+  t.true(getPredicate('mod', [3,2])(5), 'divisible this: (remainder) 2');
 
-  t.equal(getPredicate('oi', 'foo')({a: 'foo', b: 'bar'}), true, 'object includes: top key left');
-  t.equal(getPredicate('oi', 'foo')({a: 'bar', b: 'foo'}), true, 'object includes: top key right');
-  t.equal(getPredicate('oi', 'foo')({a: {a: 'foo'}, b: {a: 'bar'}}), true, 'object includes: deep key left');
-  t.equal(getPredicate('oi', 'foo')({a: {a: 'bar'}, b: {a: 'foo'}}), true, 'object includes: deep key right');
-  t.equal(getPredicate('oi', 'foo')({a: ['bar', 'foo']}), true, 'object includes: array');
-  t.equal(getPredicate('oi', 'foo')({a: ['bar', {a: 'foo'}]}), true, 'object includes: array with object');
-  t.equal(getPredicate('oi', 'foo')('foo'), true, 'object includes: string');
-  t.equal(getPredicate('oi', 'foo')(2), false, 'object includes: number');
-  t.equal(getPredicate('oi', 'foo')([1, 2, 3]), false, 'object includes: array');
-  t.equal(getPredicate('oi', 'foo')(null), false, 'object includes: null');
-  t.equal(getPredicate('oi', 'foo')(true), false, 'object includes: boolean');
-
-  t.equal(getPredicate('noi', 'foo')({a: [{a: [1, undefined, false, {a: true, b: 'foo'}]}]}),
-    false, 'object doesnt include');
+  t.true(getPredicate('oi', 'foo')({a: 'foo', b: 'bar'}), 'object includes: top key left');
+  t.true(getPredicate('oi', 'foo')({a: 'bar', b: 'foo'}), 'object includes: top key right');
+  t.true(getPredicate('oi', 'foo')({a: {a: 'foo'}, b: {a: 'bar'}}), 'object includes: deep key left');
+  t.true(getPredicate('oi', 'foo')({a: {a: 'bar'}, b: {a: 'foo'}}), 'object includes: deep key right');
+  t.true(getPredicate('oi', 'foo')({a: ['bar', 'foo']}), 'object includes: array');
+  t.true(getPredicate('oi', 'foo')({a: ['bar', {a: 'foo'}]}), 'object includes: array with object');
+  t.true(getPredicate('oi', 'foo')('foo'), 'object includes: string');
+  t.false(getPredicate('oi', 'foo')(2), 'object includes: number');
+  t.false(getPredicate('oi', 'foo')([1, 2, 3]), 'object includes: array');
+  t.false(getPredicate('oi', 'foo')(null), 'object includes: null');
+  t.false(getPredicate('oi', 'foo')(true), 'object includes: boolean');
+  t.false(getPredicate('noi', 'foo')(
+    {a: [{a: [1, undefined, false, {a: true, b: 'foo'}]}]}
+  ), 'object doesnt include');
 });
