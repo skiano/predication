c648b50ff9c2bd7db23fea098f070e8426b28ab7
Gregory Skiano <gregoryskiano@gmail.com>
2017-05-09 10:43:47 -0700

    cleaning

--- src/helpers.js
+++ src/helpers.js
@@ -17,11 +17,6 @@ export const or = predicates => v => {
 /** undefined should be false, not reversed! */
 export const not = predicate => v => predicate(v) === false ? true : false;
 
-export const isDictionary = obj => {
-  const type = typeof obj;
-  return type === 'object' && !Array.isArray(obj) && type !== 'function' && !!obj;
-};
-
 export const isString = v => typeof v === 'string';
 
 export const strIncludes = (v, c) => c.toLowerCase().includes(v.toLowerCase());
@@ -29,6 +24,11 @@ export const includes = (v, c) => isString(c) ? strIncludes(v, c) : c.includes(v
 
 export const mod = (v, c) => v % c === 0;
 export const modR = (v, [denom, remainder]) => v % denom === remainder;
+
+export const isDictionary = obj => {
+  const type = typeof obj;
+  return type === 'object' && !Array.isArray(obj) && type !== 'function' && !!obj;
+};
 
 export const objectIncludesString = (o, c) => {
   if (isString(o) && strIncludes(c, o)) {
