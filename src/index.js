50b714124bbb5980ec6fc686d39847e5cea84742
Gregory Skiano <gregoryskiano@gmail.com>
2017-05-10 14:41:57 -0700

    cleaning up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import { predication } from './predication';
 export * from './registry';
 export * from './evaluation';
 export * from './predication';
-export * from './filtration';
 
 export default predication;
