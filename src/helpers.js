d1297431ffc0f76d9b2267b5a1bfcff18fc31cba
Gregory Skiano <gregoryskiano@gmail.com>
2017-05-10 14:27:41 -0700

    refactoring helpers to use native methods

--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,9 @@
-export const and = predicates => v => {
-  let p;
-  for (p = 0; p < predicates.length; p += 1) {
-    if (!predicates[p](v)) return false;
-  }
-  return true;
-};
-
-export const or = predicates => v => {
-  let p;
-  for (p = 0; p < predicates.length; p += 1) {
-    if (predicates[p](v)) return true;
-  }
-  return false;
-};
-
 /** undefined should be false, not reversed! */
 export const not = predicate => v => predicate(v) === false ? true : false;
+export const and = predicates => v => predicates.every(p => p(v));
+export const or = predicates => v => predicates.some(p => p(v));
 
 export const isString = v => typeof v === 'string';
-
 export const strIncludes = (v, c) => c.toLowerCase().includes(v.toLowerCase());
 export const includes = (v, c) => isString(c) ? strIncludes(v, c) : c.includes(v);
 
@@ -30,24 +15,9 @@ export const isDictionary = obj => {
   return type === 'object' && !Array.isArray(obj) && type !== 'function' && !!obj;
 };
 
-export const objectIncludesString = (o, c) => {
-  if (isString(o) && strIncludes(c, o)) {
-    return true;
-  } else if (isDictionary(o)) {
-    // no map because so break as soon as possible
-    for (let k in o) {
-      if (o.hasOwnProperty(k)) {
-        if (objectIncludesString(o[k], c)) return true;
-      }
-    }
-    return false;
-  } else if (Array.isArray(o)) {
-    // no map because so break as soon as possible
-    for (let i = 0; i < o.length; i += 1) {
-      if (objectIncludesString(o[i], c)) return true;
-    }
-    return false;
-  } else {
-    return false;
-  }
-};
+export const objectIncludesString = (o, str) => (
+  (isString(o) && strIncludes(str, o)) ||
+  (isDictionary(o) && Object.keys(o).some(k => objectIncludesString(o[k], str))) ||
+  (Array.isArray(o) && o.some(v => objectIncludesString(v, str))) ||
+  false
+);
