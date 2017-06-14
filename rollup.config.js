8dd5eca67fe7e85c9b70c32c3487a593a889753c
Gregory Skiano <gregoryskiano@gmail.com>
2017-06-13 18:42:36 -0700

    adding license banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import license from 'rollup-plugin-license';
 import resolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
 
@@ -5,6 +7,14 @@ export default {
   entry: 'src/index.js',
   format: 'umd',
   moduleName: 'predication',
-  plugins: [ resolve(), buble() ],
+  plugins: [
+    resolve(),
+    buble(),
+    license({
+      banner: {
+        file: path.join(__dirname, 'banner.txt')
+      }
+    })
+  ],
   dest: 'dist/predication.js'
 };
